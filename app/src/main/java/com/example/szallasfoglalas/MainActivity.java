04fba6ca0c5462a5690218edb6e5aa96c30cc28c
Donát Fehér <50624191+Dodni@users.noreply.github.com>
2022-04-07 23:49:37 +0200

    datepicker is ready

--- app/src/main/java/com/example/szallasfoglalas/MainActivity.java
+++ app/src/main/java/com/example/szallasfoglalas/MainActivity.java
@@ -2,16 +2,18 @@ package com.example.szallasfoglalas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener{
 
     private EditText destinationEditText;
     private Button checkInButton;
@@ -20,6 +22,8 @@ public class MainActivity extends AppCompatActivity {
     private Spinner roomSpinner;
     private CheckBox businessCheckBox;
     private Button searchButton;
+
+    private boolean isCheckInDateSelected = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,16 +53,41 @@ public class MainActivity extends AppCompatActivity {
             }
         );
 
-        initAdultSpinner();
+        initSpinner(R.array.number_of_adults,adultSpinner);
+        initSpinner(R.array.number_of_rooms, roomSpinner);
+
+        checkInButton.setOnClickListener(v -> {
+            isCheckInDateSelected = true;
+            DatePickerFragment fragment = new DatePickerFragment();
+            fragment.show(getSupportFragmentManager(),"checkInDate");
+        });
+
+        checkOutButton.setOnClickListener(v -> {
+            isCheckInDateSelected = false;
+            DatePickerFragment fragment = new DatePickerFragment();
+            fragment.show(getSupportFragmentManager(),"checkOutDate");
+        });
     }
 
-    private void initAdultSpinner() {
+    private void initSpinner(int resId, Spinner spinner) {
         // lista megjelenítéséhez kell egy adapter
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.number_of_adults,
+                resId,
                 android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        adultSpinner.setAdapter(adapter);
+        spinner.setAdapter(adapter);
+    }
+
+    @Override
+    public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+        String date = year + "-" + (month+1) + "-" + day;
+
+        if(isCheckInDateSelected){
+            checkInButton.setText(date);
+        } else {
+            checkOutButton.setText((date));
+        }
+
     }
 }
